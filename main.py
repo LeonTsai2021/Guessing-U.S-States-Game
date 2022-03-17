ee48bc53436760439d1e1a76e4abd2eb5cc03d6f
Leon Tsai <deathknight285@gmail.com>
2022-03-17 23:43:49 +0800

    Update 2022/03/17

--- main.py
+++ main.py
@@ -16,13 +16,11 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States correct", prompt="What's another state's name?").title()
     
     if answer_state=="F":
-        missing_states=[]
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states=[state for state in all_states if state not in guessed_states]
         new_data=pandas.DataFrame(missing_states)
         new_data.to_csv("states_missing.csv")
         break   
+    
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
